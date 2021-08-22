5d4b8f552b3f06332cddee10e7bda74aab61b60f
Miiya <40447362+musicangora@users.noreply.github.com>
2021-08-23 03:00:08 +0900

    Unsplashから背景画像を取得

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 
 import Clock from './components/Clock';
 
-
+const requestURL = "https://source.unsplash.com/category/nature/1920x1279";
 
 const App = () => {
+  const background = {
+    backgroundImage: `url(${requestURL})`
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={background}>
+      <div className="box">
       <Clock />
+      </div>
     </div>
   );
 }

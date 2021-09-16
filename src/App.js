92b1abee55e5025d2d23af02294974ccaf683a3c
Miiya <40447362+musicangora@users.noreply.github.com>
2021-09-16 17:31:57 +0900

    fetchで画像を取得する処理を書いたが、APIアクセス数が増えるので一旦コメントアウト

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 import Clock from './components/Clock';
 
 const requestURL = "https://source.unsplash.com/category/nature/1920x1279";
 
+
 const App = () => {
+  // const [bgImages, setBgImages] = useState([]);
+
+  // useEffect(() => {
+  //   fetch("https://api.unsplash.com/photos/random/?client_id=PJzwryU_MjO2KYol7xvCywFr5q127ePjofi9P8qbKw0&count=10")
+  //     .then(response => {
+  //       if (response.ok) {
+  //         return response.json();
+  //       } else {
+  //         setBgImages([requestURL]);
+  //         return Promise.reject();
+  //       }
+  //     })
+  //     .then(result => {
+  //       setBgImages(result.map((obj) => obj.urls.regular))
+  //     });
+  // }, []);
+
+  // const setBgImage = (images) => {
+  //   return images[Math.floor(Math.random()*bgImages.length)]
+  // }
+  // console.log(setBgImage(bgImages))
+
   const background = {
     backgroundImage: `url(${requestURL})`
   }
